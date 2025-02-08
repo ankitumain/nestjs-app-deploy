7db5ccc009c5a3ccf8a7d527bfc75bee2766f6f3
ankitumain <ankit.mittal@umain.com>
2025-02-08 11:35:41 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: 'https://nestream-web.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
